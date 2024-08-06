363a9b44e8965ad52f7dcf1587d5266a6cf6d92a
Vitor Parras <vitorhugoparras@hotmail.com>
2024-08-05 23:31:59 -0300

    88.merge-sorted-array

---
+++ Easy/88.merge-sorted-array.cs
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode id=88 lang=csharp
+ *
+ * [88] Merge Sorted Array
+ */
+
+// @lc code=start
+public class Solution
+{
+    public void Merge(int[] nums1, int m, int[] nums2, int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            nums1[m] = nums2[i];
+            m++;
+        }
+
+        Array.Sort(nums1);
+    }
+}
+// @lc code=end
+
